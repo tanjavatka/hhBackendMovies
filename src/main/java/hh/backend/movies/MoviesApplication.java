39237c80b9119a1b0d7b1fb576d22a02f9d1aba5
Tanja Vatka <tanja.vatka@hotmail.com>
2026-04-06 00:52:21 +0300

    lisätty testi koodia. EI toimi vielä.

--- src/main/java/hh/backend/movies/MoviesApplication.java
+++ src/main/java/hh/backend/movies/MoviesApplication.java
@@ -1,7 +1,16 @@
 package hh.backend.movies;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import hh.backend.movies.domain.Movie;
+import hh.backend.movies.domain.MovieRepository;
+import hh.backend.movies.domain.ReviewRepository;
+import hh.backend.movies.domain.User;
+import hh.backend.movies.domain.UserMovieReview;
+import hh.backend.movies.domain.UserRepository;
 
 @SpringBootApplication
 public class MoviesApplication {
@@ -10,4 +19,16 @@ public class MoviesApplication {
 		SpringApplication.run(MoviesApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner createDemoRows(MovieRepository movieRepo, UserRepository userRepo,
+			ReviewRepository reviewRepo) {
+		return (args) -> {
+
+			movieRepo.save(new Movie("Harry Potter", 2001, "Fiction"));
+
+			// userRepo.save(new User("Tanja", "tn", "**32*"));
+
+			// reviewRepo.save(new UserMovieReview(5, "watched", "super hyvä"));
+		};
+	}
 }
