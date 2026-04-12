e3d8110defa7ff1ed73c2833a2395a98b39c726a
Tanja Vatka <tanja.vatka@hotmail.com>
2026-04-12 23:18:19 +0300

    poistettu / nimeä muutettu ko luokista/tiedostoista.

--- src/main/java/hh/backend/movies/domain/ReviewRepository.java
+++
@@ -1,9 +0,0 @@
-package hh.backend.movies.domain;
-
-import java.util.List;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface ReviewRepository extends CrudRepository<UserMovieReview, Long> {
-  List<UserMovieReview> findByStatus(String status);
-}
