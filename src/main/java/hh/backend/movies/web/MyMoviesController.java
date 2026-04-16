3cc395c7c0ed246ed9b0e6ea928e522f33da0dc1
Tanja Vatka <tanja.vatka@hotmail.com>
2026-04-16 18:48:48 +0300

    lisätty validation toiminto, korjattu paljon bugeja ja siistitty koodia (tarpeeton koodi otettu pois).

--- src/main/java/hh/backend/movies/web/MyMoviesController.java
+++ src/main/java/hh/backend/movies/web/MyMoviesController.java
@@ -4,15 +4,17 @@ import hh.backend.movies.domain.Movie;
 import hh.backend.movies.domain.MovieRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import hh.backend.movies.domain.MyMovieReview;
 import hh.backend.movies.domain.MyMovieReviewRepository;
 import hh.backend.movies.domain.RatingEnum;
 import hh.backend.movies.domain.WatchingStatus;
+import jakarta.validation.Valid;
 
 @Controller
 public class MyMoviesController {
@@ -36,23 +38,40 @@ public class MyMoviesController {
   @GetMapping("/editreview/{id}")
   public String editMovieReview(@PathVariable("id") Long reviewId, Model model) {
 
-    model.addAttribute("moviereview", myMovieRepo.findById(reviewId));
+    model.addAttribute("moviereview", myMovieRepo.findById(reviewId).orElse(null));
+
+    model.addAttribute("ratings", RatingEnum.values());
+    model.addAttribute("statuses", WatchingStatus.values());
 
     return "editreview"; // editreview.html
   }
 
-  // Save a movie review
+  // Save a new movie review
   @PostMapping("/savereview")
-  public String saveMovieReview(@RequestParam Long movieId, MyMovieReview myMovieReview) {
+  public String saveReview(@Valid @ModelAttribute("moviereview") MyMovieReview myMovieReview,
+      BindingResult bindingResult, Model model) {
 
-    Movie movie = movieRepository.findById(movieId).get();
-    myMovieReview.setMovie(movie);
+    // DEBUGGAUS
+    System.out.println("SAVE REVIEW TRIGGERED");
+    System.out.println("Movie: " + myMovieReview.getMovie());
+    System.out.println("ERRORS: " + bindingResult.getAllErrors());
+
+    // VALIDATION: jos error formin täytössä
+    // -> error viesti ilmoitus ja jää addmyreview formiin.
+    if (bindingResult.hasErrors()) {
+      model.addAttribute("movies", movieRepository.findAll());
+      model.addAttribute("ratings", RatingEnum.values());
+      model.addAttribute("statuses", WatchingStatus.values());
+
+      return "addmyreview";
+    }
 
     myMovieRepo.save(myMovieReview);
 
     return "redirect:/mymovies"; // mymovies.html
   }
 
+  // Add a new movie review
   @GetMapping("/addmoviereview")
   public String addMovieReview(Model model) {
 
@@ -62,5 +81,30 @@ public class MyMoviesController {
     model.addAttribute("statuses", WatchingStatus.values());
 
     return "addmyreview"; // addmyreview.html
+  }
+
+  // Add a movie review by id => EI KÄYTÖSSÄ VIELÄ
+  @GetMapping("/addmoviereview/{id}")
+  public String addMovieReview(@PathVariable("id") Long movieId, Model model) {
+
+    MyMovieReview review = new MyMovieReview();
+
+    Movie movie = movieRepository.findById(movieId).orElse(null);
+    review.setMovie(movie);
+
+    model.addAttribute("moviereview", review);
+    model.addAttribute("ratings", RatingEnum.values());
+    model.addAttribute("statuses", WatchingStatus.values());
+
+    return "addmyreview"; // addmyreview.html
+  }
+
+  // delete movie review by id
+  @GetMapping("/deletereview/{id}")
+  public String deleteReview(@PathVariable("id") Long reviewId, Model model) {
+
+    myMovieRepo.deleteById(reviewId);
+
+    return "redirect:/mymovies"; // mymovies.html
   }
 }
