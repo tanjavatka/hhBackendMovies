483908a2a9edcc58180aa8b4dd34c9ff7f5d2387
Tanja Vatka <tanja.vatka@hotmail.com>
2026-04-17 17:14:41 +0300

    muokattu securityyn sopivaksi. korjattu bugeja.

--- src/main/java/hh/backend/movies/domain/MyMovieReviewRepository.java
+++ src/main/java/hh/backend/movies/domain/MyMovieReviewRepository.java
@@ -5,5 +5,8 @@ import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 
 public interface MyMovieReviewRepository extends CrudRepository<MyMovieReview, Long> {
-  List<MyMovieReview> findByReviewId(Long reviewId);;
+  List<MyMovieReview> findByReviewId(Long reviewId);
+
+  // tarkistus deletemovie kohtaa varten
+  boolean existsByMovie_MovieId(Long movieId);
 }
