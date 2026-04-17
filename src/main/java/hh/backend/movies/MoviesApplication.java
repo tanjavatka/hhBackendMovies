9452d2ac3ab64f3deb3993a7d93a545c978392d0
Tanja Vatka <tanja.vatka@hotmail.com>
2026-04-17 17:15:07 +0300

    lisätty testi user ja admin.

--- src/main/java/hh/backend/movies/MoviesApplication.java
+++ src/main/java/hh/backend/movies/MoviesApplication.java
@@ -13,9 +13,16 @@ import hh.backend.movies.domain.RatingEnum;
 import hh.backend.movies.domain.User;
 import hh.backend.movies.domain.UserRepository;
 import hh.backend.movies.domain.WatchingStatus;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class MoviesApplication {
+
+	private final BCryptPasswordEncoder passwordEncoder;
+
+	MoviesApplication(BCryptPasswordEncoder passwordEncoder) {
+		this.passwordEncoder = passwordEncoder;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MoviesApplication.class, args);
@@ -29,7 +36,8 @@ public class MoviesApplication {
 			Movie movie = movieRepo.save(new Movie("Harry Potter", 2001, "Fiction"));
 			Movie movie2 = movieRepo.save(new Movie("Rottatouille", 2008, "Animation"));
 
-			User user = userRepo.save(new User("Tanja", "tn", "**32*", "user"));
+			User user = userRepo.save(new User("User", "user", (passwordEncoder.encode("user123")), "ROLE_USER"));
+			User admin = userRepo.save(new User("Admin", "admin", (passwordEncoder.encode("admin123")), "ROLE_ADMIN"));
 
 			myreviewRepo.save(new MyMovieReview(movie, RatingEnum.Five, WatchingStatus.Watched, "Super hyvä"));
 			myreviewRepo.save(new MyMovieReview(movie2, RatingEnum.Five, WatchingStatus.Watched, "Hauska"));
