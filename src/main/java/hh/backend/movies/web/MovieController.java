3f67fc8581eacb4ef0e4e56faf673da11181476a
Tanja Vatka <tanja.vatka@hotmail.com>
2026-04-17 17:12:42 +0300

    lisätty security.

--- src/main/java/hh/backend/movies/web/MovieController.java
+++ src/main/java/hh/backend/movies/web/MovieController.java
@@ -6,21 +6,26 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import hh.backend.movies.domain.Movie;
 import hh.backend.movies.domain.MovieRepository;
+import hh.backend.movies.domain.MyMovieReviewRepository;
 import jakarta.validation.Valid;
 
 @Controller
 public class MovieController {
   private MovieRepository movieRepository;
+  private MyMovieReviewRepository reviewRepository;
 
-  public MovieController(MovieRepository movieRepo) {
+  public MovieController(MovieRepository movieRepo, MyMovieReviewRepository revRepo) {
     this.movieRepository = movieRepo;
+    this.reviewRepository = revRepo;
+
   }
 
   // list all movies from database
-  @GetMapping("/movies")
+  @GetMapping({ "/movies", "/" })
   public String getMovies(Model model) {
 
     model.addAttribute("movies", movieRepository.findAll());
@@ -50,10 +55,26 @@ public class MovieController {
     return "redirect:/movies"; // movies.html
   }
 
+  // addmovie form aukee
   @GetMapping("/addmovie")
   public String addMovie(Model model) {
 
     model.addAttribute("movie", new Movie());
     return "addmovie"; // addmovie.html
+  }
+
+  // delete movie by id
+  @GetMapping("/deletemovie/{id}")
+  public String deleteMovie(@PathVariable("id") Long movieId, RedirectAttributes redirectAttributes) {
+
+    // tarkistetaan onko elokuvalla arvosteluja
+    if (reviewRepository.existsByMovie_MovieId(movieId)) {
+      redirectAttributes.addFlashAttribute("error", "Cannot delete movie with reviews");
+      return "redirect:/movies";
+    }
+
+    movieRepository.deleteById(movieId);
+
+    return "redirect:/movies"; // movies.html
   }
 }
