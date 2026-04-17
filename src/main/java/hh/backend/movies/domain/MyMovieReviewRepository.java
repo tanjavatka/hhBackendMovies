650857e07f901e34238790f14f84fd8113bb25ec
Tanja Vatka <tanja.vatka@hotmail.com>
2026-04-17 22:30:29 +0300

    lisätty lista jossa vain käyttäjä kohtaiset arvostelut.

--- src/main/java/hh/backend/movies/domain/MyMovieReviewRepository.java
+++ src/main/java/hh/backend/movies/domain/MyMovieReviewRepository.java
@@ -5,7 +5,12 @@ import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 
 public interface MyMovieReviewRepository extends CrudRepository<MyMovieReview, Long> {
+
+  // Listaa kaikki arvostelut (Id:n mukaan)
   List<MyMovieReview> findByReviewId(Long reviewId);
+
+  // listataan arvostelut käyttäjän mukaan
+  List<MyMovieReview> findByUserUsername(String username);
 
   // tarkistus deletemovie kohtaa varten
   boolean existsByMovie_MovieId(Long movieId);
